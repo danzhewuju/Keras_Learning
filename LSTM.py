b7378e630a5c813983541edba46d7296242919cf
Alex <1668926294@qq.com>
2018-11-20 19:16:57 +0800

    LSTM网络的构造，以及使用

--- LSTM.py
+++ LSTM.py
@@ -14,7 +14,7 @@ from Unit import *
 
 def learning_lstm():
     max_feature = 10000
-    maxlen = 64
+    maxlen = 64#设置最长的序列，如果长与这个序列会被阶段，短与这个序列会被填充
     (x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=max_feature)
     x_train = preprocessing.sequence.pad_sequences(x_train, maxlen=maxlen)
     x_test = preprocessing.sequence.pad_sequences(x_test, maxlen=maxlen)
@@ -31,3 +31,6 @@ def learning_lstm():
 
     history = model.fit(x_train, y_train, epochs=10, batch_size=128, validation_split=0.2)
     Draw(history)
+
+
+learning_lstm()
